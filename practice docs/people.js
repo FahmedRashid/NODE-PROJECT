d4f53ea3f570dccf92868e691e686e4957fb6821
fahmed <fahmed7921@gmail.com>
2025-04-12 14:18:33 -0500

    old

---
+++ practice docs/people.js
@@ -0,0 +1,8 @@
+const people = ['fahmed','rashid','anu']
+const ages = [20,25,10]
+
+console.log(people);
+
+module.exports = {
+    people, ages
+}
---
+++ practice docs/test.js
@@ -0,0 +1,10 @@
+// const name = 'Fahmed';
+
+// console.log(name);
+
+const treat = (name) =>{
+    console.log(`The name of the treat would be ${name}`);
+
+}
+treat('fahmed');
+treat('rashid');
