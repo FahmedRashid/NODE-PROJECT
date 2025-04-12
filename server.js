15fb8509b6a6f5d18b8c7d23fcba0b2816a08fe6
fahmed <fahmed7921@gmail.com>
2025-04-12 15:03:47 -0500

    nodemon and lodash

--- server.js
+++ server.js
@@ -1,20 +1,51 @@
 const fs = require('fs');
 const http = require('http')
+const _= require('lodash');
 
 const server = http.createServer((req, res) =>{
-    console.log(req.url, req.method);
+
+    // lodash
+    const num = _.random(0, 20);
+    console.log(num);
+
+    const greet = _.once(() =>{
+        console.log('hello');
+    });
+    greet();
+    greet();
 
     // -- set header content type --
     res.setHeader('Content-Type', 'text/html')
 
+    let path = './views/';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            res.statusCode =200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode =200;
+            break;
+        case '/about-us':
+            res.statusCode =301;
+            res.setHeader('Location', 'about');
+            res.end()
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
 // --send an html files--
-    fs.readFile('./views/index.html', (err, data) =>{
+    fs.readFile(path, (err, data) =>{
         if(err){
             console.log(err);
             res.end();
         }else {
-           // res.write(data); //or add tata here res.end(data);
-            res.end(data);
+           // res.write(data); //or add tata here res.end(data); 
+           res.end(data);
+
         }
     })
 
