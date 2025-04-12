2f6a671fb17a5b92d1187d825362ddff97de347e
fahmed <fahmed7921@gmail.com>
2025-04-12 16:04:38 -0500

    using ejs to handle all the routes. Removed the html files and updated them .ejs

--- app.js
+++ app.js
@@ -3,30 +3,30 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view engine ejs
+app.set('view engine', 'ejs'); // by default ejs knows to check the view folder
+
 // listen for request
 app.listen(3000);
 
 app.get('/', (req, res) =>{
    // res.send('<p>home page</p>');
-   res.sendFile('./views/index.html', { root: __dirname});
+   res.render('index');
 })
 app.get('/about', (req, res) =>{
     //res.send('<p>About Page</p>');
-    res.sendFile('./views/about.html', { root: __dirname});
+    res.render('about');
 })
 app.get('/import', (req, res) => {
     //Import page
-    res.sendFile('./views/import.html', {root: __dirname})
+    res.render('import');
 })
 
-
-
-// redirects
-app.get('/about-us', (req, res) =>{
-    res.redirect('/about');
+app.get('/blogs/create', (req, res) =>{
+    res.render('create');
 })
 
 // 404 page the app.use() needs to be used at the last.
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404');
 })
