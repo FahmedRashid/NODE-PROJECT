9a04839c08a2c29f333fc472cd66825c6a71ace9
fahmed <fahmed7921@gmail.com>
2025-04-13 20:31:46 -0500

    Connection to Mongoose db completed. used dbURI in the app

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+
 //express app
 const app = express();
+
+//connect to Mongodb
+const dbURI = 'mongodb+srv://fahmed:fahmed123@false-blogs.da5wy9v.mongodb.net/blogDb?retryWrites=true&w=majority&appName=False-Blogs';
+mongoose.connect(dbURI) //, { useNewUrlParser: true, useUnifiedTopology: true} // console texts shows warnings
+    .then((result)=> app.listen(3000)) // listen for request
+    .catch((err) => console.log(err));
 //register view engine ejs
 app.set('view engine', 'ejs'); // by default ejs knows to check the view folder
-// listen for request
-app.listen(3000);
+
+
 
 // middleware static files like css templates.
 app.use(express.static('public'));
